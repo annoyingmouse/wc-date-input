f38e437561fc5aec7aee91b9b6fe92103bacb3eb
Dominic Myers <annoyingmouse@gmail.com>
2023-06-10 16:52:08 +0100

    style(wc-date-input.js): fix typo in static property name from formAssocaited to formAssociated feat(wc-date-input.js): add input validation for day, month, and year inputs feat(wc-date-input.js): add updateValue method to update day, month, and year values and call checkValidity method for each input chunk refactor(wc-date-input.js): remove unused css classes and styles from css method docs(wc-date-input.js): add comments to checkValidity and updateValue methods

--- wc-date-input.js
+++ wc-date-input.js
@@ -1,12 +1,13 @@
 class WCDateInput extends HTMLElement {
-  #day = null
-  #month = null
-  #year = null
+  #day = ''
+  #month = ''
+  #year = ''
   #dayText = 'Day'
   #monthText = 'Month'
   #yearText = 'Year'
 
-  static formAssocaited = true
+  static formAssociated = true
+
 
 
   static get observedAttributes() {
@@ -41,52 +42,59 @@ class WCDateInput extends HTMLElement {
     this.#day = this.shadow.querySelector('#day')
     this.#month = this.shadow.querySelector('#month')
     this.#year = this.shadow.querySelector('#year')
-    this.#day.addEventListener('change', () => this.updateValue())
-    this.#month.addEventListener('change', () => this.updateValue())
-    this.#year.addEventListener('change', () => this.updateValue())
+    this.#day.addEventListener('change', () => this.updateValue('day'))
+    this.#month.addEventListener('change', () => this.updateValue('month'))
+    this.#year.addEventListener('change', () => this.updateValue('year'))
   }
 
   get css() {
     return `
       <style>
+        .date-input {
+          display: grid; 
+          grid-template-columns: 2.75em 2.75em 4em;
+          gap: 0% 20px; 
+          font-family: arial,sans-serif;
+        }
         .date-input:after {
           content: "";
           display: block;
           clear: both;
         }
-        .date-input__item {
-          display: inline-block;
-          margin-right: 20px;
-          margin-bottom: 0;
-        }
         .form-group {
           display: flex;
           flex-direction: column;
         }
-        label {
-          font-weight: 400;
-          color: #0b0c0c;
-          margin-bottom: 5px;
+        .form-group:after {
+          content: "";
           display: block;
+          clear: both;
         }
-        .date-input__input {
-          margin-bottom: 0;
+        label {
+          display: block;
+          font-weight: 400;
+          font-size: 19px;
+          line-height: 25px;
+          margin-bottom: 5px;
+          text-size-adjust: 100%;
+          font-smoothing: antialiased;
         }
-        
-        .input--width-2 {
-          max-width: 2.75em;
+        input {
+          height: 2.5rem;
+          margin-top: 0;
+          line-height: 1.5;
+          border: 2px solid #0b0c0c;
+          padding: 5px;
+          border-radius: 0;
         }
-        
-        
-        @media (min-width: 40.0625em)
-          .form-group {
-            margin-bottom: 30px;
-          }
-          .date-input__label {
-            font-size: 19px;
-            font-size: 1.1875rem;
-            line-height: 1.3157894737;
-          }
+        input.error {
+          border-color: #D4352C;
+        }
+        input:focus {
+          outline: 3px solid #FFDD00;
+          outline-offset: 0;
+          box-shadow: inset 0 0 0 2px;
+        }
       </style>
     `
   }
@@ -94,41 +102,45 @@ class WCDateInput extends HTMLElement {
   get html() {
     return `
       <div class="date-input">
-        <div class="date-input__item">
-          <div class="form-group">
-            <label for="day">
-              ${this.dayText}
-            </label>
-            <input className="date-input__input input--width-2"
-                   id="day"
-                   name="day"
-                   type="text"
-                   inputmode="numeric">
-          </div>
+        <div class="form-group">
+          <label for="day">
+            ${this.dayText}
+          </label>
+          <input id="day"
+                 name="day"
+                 type="text"
+                 min="1"
+                 max="31"
+                 value="${this.#day}"
+                 pattern="[0-9]{1,2}"
+                 inputmode="numeric"
+                 data-form-type="date,day" />
         </div>
-        <div class="date-input__item">
-          <div class="form-group">
-            <label for="month">
-              ${this.#monthText}
-            </label>
-            <input className="date-input__input input--width-2"
-                   id="month"
-                   name="month"
-                   type="text"
-                   inputmode="numeric"/>
-          </div>
+        <div class="form-group">
+          <label for="month">
+            ${this.#monthText}
+          </label>
+          <input id="month"
+                 name="month"
+                 type="text"
+                 min="1"
+                 max="12"
+                 value="${this.#month}"
+                 pattern="[0-9]{1,2}"
+                 inputmode="numeric"
+                 data-form-type="date,month" />
         </div>
-        <div class="date-input__item">
-          <div class="form-group">
-            <label for="year"/>
-              ${this.#yearText}
-            </label>
-            <input class="date-input__input input--width-4"
-                   id="year"
-                   name="year"
-                   type="text"
-                   inputmode="numeric"/>
-          </div>
+        <div class="form-group year-inout">
+          <label for="year"/>
+            ${this.#yearText}
+          </label>
+          <input id="year"
+                 name="year"
+                 type="text"
+                 value="${this.#year}"
+                 pattern="[0-9]{4}"
+                 inputmode="numeric" 
+                 data-form-type="date,year" />
         </div>
       </div>
     `
@@ -144,9 +156,26 @@ class WCDateInput extends HTMLElement {
     }
   }
 
-  updateValue() {
-    const date = new Date(this.#year.value, this.#month.value - 1, this.#day.value)
-    this.value = date.toISOString().split('T')[0]
+  checkValidity(chunk) {
+    console.log('checkValidity called', chunk)
+  }
+
+  updateValue(chunk) {
+    console.log('updateValue called', chunk)
+    if (chunk === 'day') {
+      this.#day = this.#day.value
+      this.checkValidity(chunk)
+    }
+    if (chunk === 'month') {
+      this.#month = this.#month.value
+      this.checkValidity(chunk)
+    }
+    if (chunk === 'year') {
+      this.#year = this.#year.value
+      this.checkValidity(chunk)
+    }
+    // const date = new Date(this.#year.value, this.#month.value - 1, this.#day.value)
+    // this.value = date.toISOString().split('T')[0]
   }
 
   /**
@@ -156,6 +185,11 @@ class WCDateInput extends HTMLElement {
    */
   get value() {
     return this.hasAttribute('value') ? new Date(this.getAttribute('value')) : new Date()
+  }
+
+  set value(value) {
+    this.setAttribute('value', value)
+    this.internals.setFormValue(value)
   }
 
   /**
